ce04b5ef835004872aa4c463c9f0c0fa1dc765cf
Harry Gueguen <harry.gueguen@gmail.com>
2022-06-28 11:16:18 +0200

    Style on flash messages (info and alert)

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,39 @@
+
+Rails.application.reloader.to_prepare do
+  class TurboFailureApp < Devise::FailureApp
+    def respond
+      if request_format == :turbo_stream
+        redirect
+      else
+        super
+      end
+    end
+
+    def skip_format?
+      %w(html turbo_stream */*).include? request_format.to_s
+    end
+  end
+
+  class TurboController < ApplicationController
+    class Responder < ActionController::Responder
+      def to_turbo_stream
+        controller.render(options.merge(formats: :html))
+      rescue ActionView::MissingTemplate => error
+        if get?
+          raise error
+        elsif has_errors? && default_action
+          render rendering_options.merge(formats: :html, status: :unprocessable_entity)
+        else
+          redirect_to navigation_location
+        end
+      end
+    end
+
+    self.responder = Responder
+    respond_to :html, :turbo_stream
+  end
+end
+
 # frozen_string_literal: true
 
 # Assuming you have not yet modified this file, each configuration option below
@@ -9,6 +45,13 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+
+  config.parent_controller = 'TurboController'
+
+  config.warden do |manager|
+    manager.failure_app = TurboFailureApp
+  end
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
