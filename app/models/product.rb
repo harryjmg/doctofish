6654be4d6d07595dbabec28ce0378aceb5a8664c
Harry Gueguen <harry.gueguen@gmail.com>
2022-06-28 11:39:46 +0200

    Form handle error on front (reminders and products)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   has_and_belongs_to_many :diseases
 
+  validates :name, :description, :affiliation_link, presence: true
 end
--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,5 +1,5 @@
 class Reminder < ApplicationRecord
   belongs_to :user
 
-
+  validates :name, :date, :reminder_type, :repetition_recurrence, presence: true
 end
